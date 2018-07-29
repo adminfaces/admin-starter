2cb02fe89525aa869e2bf0745b5934261815a666
rmpestano <rmpestano@gmail.com>
2018-07-28 22:49:46 -0300

    updates to AF RC16

--- src/main/webapp/resources/js/starter.js
+++ src/main/webapp/resources/js/starter.js
@@ -1,10 +1,20 @@
-
 $(document).ready(function () {
+    activateMobileBar();
+});
+
+/**
+ * adds mobile loading bar to links 
+ * 
+ * @return {undefined}
+ */
+function activateMobileBar() {
     if (isMobile()) {
-        $('ul.sidebar-menu a.ui-link, ul.navbar li a.ui-link').click(function () {
+        $('ul.sidebar-menu a.ui-link, ul.navbar-nav li a.ui-link, ol.breadcrumb a').click(function () {
             $(this).prop('disabled', true);
             showBar();
-        })
+        });
     }
-});
+}
+
+
 
