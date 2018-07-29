3bcfb324faea773ac9d01003d98a12e76b2c5bd2
rmpestano <rmpestano@gmail.com>
2018-07-28 21:31:52 -0300

    perf

--- src/main/webapp/resources/js/starter.js
+++ src/main/webapp/resources/js/starter.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function () {
     if (isMobile()) {
-        $('ul.sidebar-menu a.ui-link, ul.nav navbar a.ui-link').click(function () {
+        $('ul.sidebar-menu a.ui-link, ul.navbar li a.ui-link').click(function () {
             $(this).prop('disabled', true);
             showBar();
         })
